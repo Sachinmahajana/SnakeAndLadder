6a8a64eb2efebb88132ce214d8fbb92eef2e2e29
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-27 19:36:46 +0530

    [Sachin] Adding Welcome message in program

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Program");
+            Console.ReadLine();
+        }
+    }
+}
